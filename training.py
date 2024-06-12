dacdfba5a213123ddbc0a8fcbe9b63dfea36d8ea
HannaFarha <johnnyfarha1@gmail.com>
2024-06-12 13:19:35 +0200

    range

--- training.py
+++ training.py
@@ -1,7 +1,10 @@
+
+for i in range(1,10,2):
+    print(i)
 while True:
-    line = input("command:>")
-    if line.startswith("#"):
-        continue
-    if line=="Done":
-        break
-    print(line)
+        line = input("command:>")
+        if line.startswith("#"):
+            continue
+        if line == "Done":
+            break
+        print(line)
