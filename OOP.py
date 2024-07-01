5647dff287935fddb15674173c9c1de94be7f8d2
HannaFarha <johnnyfarha1@gmail.com>
2024-07-01 11:51:56 +0200

    Inheritance

--- OOP.py
+++ OOP.py
@@ -6,6 +6,15 @@ class Employee:
     def greeting(self):
         print("Hi, my name is {} ".format(self.name))
 
+
+class secundEmployee(Employee):
+    def local_greeting(self):
+        print("Ola")
+        self.greeting()
+class thirdEmployee(Employee):
+    def local_greeting(self):
+        print("Ola Ola")
+        self.greeting()
 class Hotel:
     def __init__(self, name, country):
         self.name = name
@@ -21,6 +30,10 @@ e1 = Employee("Johnny",25,10000)
 e2=Employee("Hanna",42,1000)
 e3 = Employee("Johny",24,100)
 e4=Employee("Hana",43,10090)
+e5 = secundEmployee("Johnny",25,10000)
+e6= secundEmployee("Hanna",42,1000)
+e7 = thirdEmployee("Johny",24,100)
+e8=thirdEmployee("Hana",43,10090)
 
 h1=Hotel("ysdkk","Germany")
 h2=Hotel("ysdkko","Syria")
@@ -33,3 +46,6 @@ print("Syria sum = {}".format(sum([x.salary for x in h2.get_employees()])))
 
 print([x.salary for x in h1.get_employees()])
 print([x.salary for x in h2.get_employees()])
+
+e5.local_greeting()
+e7.local_greeting()
